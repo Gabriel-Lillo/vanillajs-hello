82868d7002ea3e930e970c7b23a444fa0f6085b3
Gabriel-Lillo <gabriel.lillovjm@gmail.com>
2021-04-14 05:12:53 +0000

    inicio2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  Document.querySelector("#excuse").innerHTML = randomNumber;
   console.log("Hello Rigo from the console!");
 };
+
+let generate;
