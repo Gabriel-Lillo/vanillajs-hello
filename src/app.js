4969d2cb92a7ee5f720987d66f9cb1d034a2457c
Gabriel-Lillo <gabriel.lillovjm@gmail.com>
2021-04-14 06:33:12 +0000

    inicio3

--- src/app.js
+++ src/app.js
@@ -6,9 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  let randomNumber = Math.random() * 10;
-  Document.querySelector("#excuse").innerHTML = randomNumber;
+  Document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
 
-let generate;
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIn = Math.floor(Math.random() * who.length);
+  let whatIn = Math.floor(Math.random() * who.length);
+  let whenIn = Math.floor(Math.random() * who.length);
+
+  return who[whoIn] + " " + what[whatIn] + " " + when[whenIn];
+};
